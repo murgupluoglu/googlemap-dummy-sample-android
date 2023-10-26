ca003afbea3f733b0ff00852bd5e4ef4d60f0c08
murgupluoglu <masadov@gmail.com>
2023-10-26 10:36:56 +0300

    build: update gradle

--- app/src/main/java/com/murgupluoglu/googlemap/MainActivity.kt
+++ app/src/main/java/com/murgupluoglu/googlemap/MainActivity.kt
@@ -1,47 +1,49 @@
 package com.murgupluoglu.googlemap
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val position = LatLng(41.015137, 28.979530)
+    private val position = LatLng(41.015137, 28.979530)
 
-    var markerOptions = MarkerOptions().position(position)
+    private var markerOptions = MarkerOptions().position(position)
 
-    lateinit var marker : Marker
+    private var marker: Marker? = null
+
+    private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mapView = findViewById(R.id.mapView)
+
         with(mapView) {
             // Initialise the MapView
             onCreate(null)
             // Set the map ready callback to receive the GoogleMap object
-            getMapAsync{
+            getMapAsync {
                 MapsInitializer.initialize(applicationContext)
                 setMapLocation(it)
             }
         }
     }
 
-    private fun setMapLocation(map : GoogleMap) {
+    private fun setMapLocation(map: GoogleMap) {
         with(map) {
             moveCamera(CameraUpdateFactory.newLatLngZoom(position, 13f))
             mapType = GoogleMap.MAP_TYPE_NORMAL
             setOnMapClickListener {
-                if(::marker.isInitialized){
-                    marker.remove()
-                }
+                marker?.remove()
                 markerOptions.position(it)
                 marker = addMarker(markerOptions)
                 Toast.makeText(this@MainActivity, "Clicked on ", Toast.LENGTH_SHORT).show()
